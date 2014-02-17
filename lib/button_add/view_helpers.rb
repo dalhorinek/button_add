3ac112d40959674aae975802d55515ba18c2aebf
Dalibor Horinek <dal@horinek.net>
2014-02-17 17:27:34 +0100

    Init pass in options

--- lib/button_add/view_helpers.rb
+++ lib/button_add/view_helpers.rb
@@ -2,8 +2,8 @@ require "button_add/version"
 
 module ButtonAdd
   module ViewHelpers
-    def link_to_js_add(name, f, association, options = {}, object_init={})
-      new_object = f.object.send(association).klass.new(object_init) 
+    def link_to_js_add(name, f, association, options = {})
+      new_object = f.object.send(association).klass.new(options[:init]) 
       id = new_object.object_id
 
       partial = options[:partial]
